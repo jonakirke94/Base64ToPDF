1161b91887ad67a8fa85538c0bc27dee5d582c95
Jonakirke <jhk@northarc.dk>
2022-03-18 21:30:50 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+var base64str = args.AsQueryable().FirstOrDefault();
+
+if (base64str == null)
+{
+    Console.WriteLine("Please insert a base64 string");
+    return;
+}
+
+byte[] fileArray = Convert.FromBase64String(base64str);
+string path = Directory.GetCurrentDirectory() + "\\" + Guid.NewGuid() + ".pdf";
+Console.WriteLine(path);
+File.WriteAllBytes(path, fileArray);
+new Process
+{
+    StartInfo = new ProcessStartInfo(path)
+    {
+        UseShellExecute = true
+    }
+}.Start();
